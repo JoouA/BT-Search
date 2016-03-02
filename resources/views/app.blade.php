8220e2e39fdee9967a14c47c864845edac1b6a0b
kslr <kslrwang@gmail.com>
2016-03-02 22:02:24 +0800

    将Google Jquery Cdn改为国内CDN服务商

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,15 @@
     <body>
         @yield('content')
 
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script type="text/javascript" src="//cdn.bootcss.com/jquery/2.2.1/jquery.min.js"></script>
+        <script>
+			var _hmt = _hmt || [];
+			(function() {
+			  var hm = document.createElement("script");
+			  hm.src = "//hm.baidu.com/hm.js?4c7136d8d9342c4f5ae909bcf2cdee7f";
+			  var s = document.getElementsByTagName("script")[0]; 
+			  s.parentNode.insertBefore(hm, s);
+			})();
+		</script>
     </body>
 </html>
